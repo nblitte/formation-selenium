8b9ded5c77481db42fa5afaeb84160b1839c51b5
nblitte <nathalie.blitte@gmail.com>
2020-07-22 09:01:49 +0200

    Update Exercice1.java

--- src/main/java/formationselenium/Orange/Exercice1.java
+++ src/main/java/formationselenium/Orange/Exercice1.java
@@ -24,13 +24,13 @@ public class Exercice1 {
   @Test
   public void test() throws Exception {
 	  //login
-    driver.get("http://universitedutest.com/OrangeHRM/symfony/web/index.php/auth/login");
+    driver.get("https://opensource-demo.orangehrmlive.com/");
     driver.findElement(By.id("txtUsername")).click();
     driver.findElement(By.id("txtUsername")).clear();
     driver.findElement(By.id("txtUsername")).sendKeys("Admin");
     driver.findElement(By.id("txtPassword")).click();
     driver.findElement(By.id("txtPassword")).clear();
-    driver.findElement(By.id("txtPassword")).sendKeys("Nantes$2020");
+    driver.findElement(By.id("txtPassword")).sendKeys("admin123");
     driver.findElement(By.id("btnLogin")).click();
     
     
